77cb86df742f04e579bd42729858d63045071027
Justin Spegele <justin.spegele@gmail.com>
2020-10-27 16:10:02 -0400

    Added collection and collectionsOverview containers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import rootReducer from './root-reducer'
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-const middlewares = []
+const middlewares = [thunk]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
